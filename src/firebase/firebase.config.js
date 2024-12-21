e109d8d749d5e03a5665d3ab1324dd1d564b51e4
RMABID <mdabidaliimran@gmail.com>
2024-12-21 21:45:00 +0600

    firebase env local add

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA0kAcA0lNmy3xfajtDgqJZ_qTKMAP1rGk",
-  authDomain: "historical-artifacts.firebaseapp.com",
-  projectId: "historical-artifacts",
-  storageBucket: "historical-artifacts.firebasestorage.app",
-  messagingSenderId: "788217614922",
-  appId: "1:788217614922:web:79e3473fb0ca8f968c4cb3",
-  measurementId: "G-BEXTXHBQCE",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
